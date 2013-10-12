2e8410bf0d5d6e26936ea94a55eb84fb0507ce72
Thomas Limp <thomas.limp@produktmacher.com>
2013-10-12 16:25:17 +0200

    working with data structures tasks - sequences

--- src/defworkshop/working_with_sequences.clj
+++ src/defworkshop/working_with_sequences.clj
@@ -1,130 +1,135 @@
 (ns defworkshop.working-with-sequences
   (:require [workshoplib.tdd :refer […]]))
 
-(defn ^:not-implemented split-head-and-tail
+(defn split-head-and-tail
   "Split head an tail of sequence by using `first` and `next` functions"
   [list]
-  (…))
+  [(first list) (rest list)])
 
-(defn ^:not-implemented get-nth
+(defn get-nth
   "Now let's write a function that retrieves nth element from the sequence. For that, you can use `nth`"
   [seq n]
-  (…))
+  (nth seq n))
 
-(defn ^:not-implemented conj-to-list
+(defn conj-to-list
   "Conj is an operator that adds element to the list. In case with lists, it
    appends items in a reverse order."
   [list item]
-  (…))
+  (conj list item))
 
-(defn ^:not-implemented conj-to-vector
+(defn conj-to-vector
   "Conj operator with vector appends to tail of vector"
   [vector item]
-  (…))
+  (conj vector item))
 
-(defn ^:not-implemented remove-from-set
+(defn remove-from-set
   "Remove an element from a set using `disj`"
   [s to-remove]
-  (…))
+  (disj s to-remove))
 
-(defn ^:not-implemented rev-concat
+(defn rev-concat
   "given two sequences `s1` and `s2`, concatenate them such that the resulting sequence starts with the last element of `s2` and ends with the first element of `s1`. Hint: there's a function called `reverse` that might be of help."
   [s1 s2]
-  (…))
+  (concat (reverse s2) (reverse s1)))
 
-(defn ^:not-implemented interleave-seqs
+(defn interleave-seqs
   "given two sequences `s1` and `s2`, interleave them, starting with the shorter sequence."
   [s1 s2]
-  (…))
+  (if (<= (count s1) (count s2))
+    (interleave s1 s2)
+    (interleave s2 s1)))
 
-(defn ^:not-implemented split-head-and-tail-destructuring
+(defn split-head-and-tail-destructuring
   "Split head and tail of sequence by destructuring the input. Here, instead of `list` (see split-head-and-tail above), use [[head & tail]] which will assign the first element to `head` and
    the rest of sequence (denoted by &) to the tail."
-  [list]
-  (…))
+  [[head & tail]]
+  [head tail])
 
-(defn ^:not-implemented contains-index
+(defn contains-index
   "`contains` is a somewhat confusing function. Usually people expect (contains? [1 2 3] 3) to return
    true, but it returns false, since it checks wether the sequence contains element with `key` (index, in
    case with vectors."
   [coll index]
-  (…))
+  (contains? coll index))
 
-(defn ^:not-implemented real-contains
+(defn real-contains
   "But if you use `some` instead of `contains` it will produce a desirable result. Let's implement a real
    contains with `some` and equality predicate"
   [coll item]
-  (…))
+  (some (fn [i] (= i item)) coll))
 
-(defn ^:not-implemented double-all
+(defn double-all
   "Double each item in sequence by using `map`."
   [l]
-  (…))
+  (map (fn [i] (* 2 i)) l))
 
-(defn ^:not-implemented add-5-to-all
+(defn add-5-to-all
   "Add 5 to each item in an array by using `map` and an anonymous function"
   [l]
-  (…))
+  (map (fn [i] (+ 5 i)) l))
 
-(defn ^:not-implemented add-5-to-all-with-for
+(defn add-5-to-all-with-for
   [l]
-  (…))
+  (for [i l] (+ 5 i)))
 
-(defn ^:not-implemented print-combinations
+(defn print-combinations
   "given two sequences `s1` and `s2`, print each combination of their elements where the elements aren't equal as a vector."
   [s1 s2]
-  (…))
+  (doseq [x s1 y s2] (when (not(= x y)) (println [x y]))))
 
-(defn ^:not-implemented split-to-pairs
+(defn split-to-pairs
   "Given a sequence, split it to pairs.
 
    You can use `partition` function for that."
   [seq]
-  (…))
+  (partition 2 seq))
 
-(defn ^:not-implemented moving-average
+(defn moving-average
   "given a sequence of numbers, calculate the moving average using `partition`."
   [nums window-size]
-  (…))
+  (map 
+    (fn [window] 
+      (/ (apply + window) (count window))) 
+    (partition window-size 1 nums)))
 
-(defn ^:not-implemented sum
+(defn sum
   "Calculate a sum of numbers in a list by using `reduce`"
   [list]
-  (…))
+  (reduce + list))
 
-(defn ^:not-implemented even-only
+(defn even-only
   "Filter even items from the list by using `filter` and `even?`"
   [l]
-  (…))
+  (filter even? l))
 
-(defn ^:not-implemented odd-only
+(defn odd-only
   "Filter out all the even items from the list, using `remove` (opposite of `filter`) and
    `even?`"
   [l]
-  (…))
+  (remove even? l))
 
 (defn ^:not-implemented sum-of-squares-of-even-numbers
   "This excercise will help you to understand threading operator in Clojure"
   [coll]
-  (…))
+  ())
 
-(defn ^:not-implemented get-part-of-vector
+(defn get-part-of-vector
   "In order to get part of the vector, you can use `subvec`."
   ([vec start]
-     (…))
+     (subvec vec start))
   ([vec start end]
-     (…)))
+     (subvec vec start end)))
 
-(defn ^:not-implemented sort-sequence
+(defn sort-sequence
   "You often need to have a sequence sorted, for that you can use `sort` function"
   [seq]
-  (…))
+  (sort seq))
 
-(defn ^:not-implemented sort-maps
+(defn sort-maps
   "If you have a sequence of maps that has a structure [{:name \"Alex\" :age 25} {:name \"Maxi\" :age 26}],
    you can use `sort-by` to sort these maps by :age key"
   [seq]
-  (…))
+  (sort-by :age seq))
 
 (defn ^:not-implemented interpolate-words
   "Given a string of format \"Hello %name%, you're %age% years old!\", and map {:name \"Alex\" :age 25},
@@ -132,10 +137,10 @@
   [string m]
   (…))
 
-(defn ^:not-implemented repeat-times
+(defn repeat-times
   "For the given `number`, return a sequence where this number is repeated `n` times"
   [number n]
-  (…))
+  (for [x (range n)] number))
 
 (defn ^:not-implemented read-and-summarize-contents
   "Given a filename of the file that holds numbers (newline-separated), write a function that parses numbers in file,
