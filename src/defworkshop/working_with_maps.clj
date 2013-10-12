50535498ea244c3101c0a09affd1adeba07d66bc
Thomas Limp <thomas.limp@produktmacher.com>
2013-10-12 16:24:46 +0200

    working with data structures tasks - maps

--- src/defworkshop/working_with_maps.clj
+++ src/defworkshop/working_with_maps.clj
@@ -5,12 +5,15 @@
 ;; Maps
 ;;
 
-(defn ^:not-implemented get-name-or-empty
+(defn get-name-or-empty
   "Write a function that would retrieve a :name from map or return `:empty` token otherwise"
   [m]
-  (…))
+  (def name (get m :name))
+  (if name
+    name
+    :empty))
 
-(defn ^:not-implemented retrieve-city
+(defn retrieve-city
   "Given hash with following structure:
 
       {:name \"Alex\"
@@ -18,59 +21,59 @@
 
    Retrieve :city from the hash"
   [m]
-  (…))
+  (get (get m :address) :city))
 
-(defn ^:not-implemented get-by-path
+(defn get-by-path
   "Given a map of {:person {:name {:first-name \"Alex\" }}} return the `:first-name` key value.
 
    You can use `get-in` for that"
   [m]
-  (…))
+  (get-in m [:person :name :first-name]))
 
-(defn ^:not-implemented add-name
+(defn add-name
   "Write a function that adds :name to the map by using `assoc`"
   [m name]
-  (…))
+  (assoc m :name name))
 
-(defn ^:not-implemented add-age
+(defn add-age
   "Write a function that adds :age to the map by using `assoc`"
   [m age]
-  (…))
+  (assoc m :age age))
 
-(defn ^:not-implemented add-name-and-age
+(defn add-name-and-age
   "Use threading `->`, `add-name` and `add-age` functions to compose a new record that has both :name and :age"
   [m name age]
-  (…))
+  (assoc (assoc m :age age) :name name))
 
-(defn ^:not-implemented remove-name
+(defn remove-name
   "Remove :name key from map by using `dissoc`"
   [m]
-  (…))
+  (dissoc m :name))
 
-(defn ^:not-implemented remove-age
+(defn remove-age
   "Remove :age key from map by using `dissoc`"
   [m]
-  (…))
+  (dissoc m :age))
 
-(defn ^:not-implemented remove-name-and-age
+(defn remove-name-and-age
   "Use threading `->`, `remove-name` and `remove-age` functions to compose a new record without :name and :age"
   [m]
-  (…))
+  (dissoc (dissoc m :age) :name))
 
-(defn ^:not-implemented map-keys
+(defn map-keys
   "Given a map, return only it's keys. You can use `keys` function for that."
   [m]
-  (…))
+  (keys m))
 
-(defn ^:not-implemented map-values
+(defn map-values
   "Given a map, return only it's values. You can use `vals` function for that."
   [m]
-  (…))
+  (vals m))
 
-(defn ^:not-implemented map-select
+(defn map-select
   "Given a map `m` and a vector of keys `ks`, return a map that only contains entries keyed by `ks`. hint: you can use `select-keys`."
   [m ks]
-  (…))
+  (select-keys m ks))
 
 (defn ^:not-implemented map-from-keys-and-vals
   "Construct a new map from keys and values. Hint: take a look at `zipmap`."
@@ -82,22 +85,24 @@
   [m k]
   (…))
 
-(defn ^:not-implemented map-merge
+(defn map-merge
   "given two maps `m1` and `m2`, merge them such that when there's identical keys in both maps, the value in `m1` will prevail in the merged map."
   [m1 m2]
-  (…))
+  (merge m2 m1))
 
-(defn ^:not-implemented merge-max
+(defn merge-max
   "given two maps `m1` and `m2` (values are all numbers), merge them such that for identical keys, the larger value prevails. try to leverage `merge-with` in order to achieve this."
   [m1 m2]
-  (…))
+  (merge-with 
+    (fn [a b] (max a b))
+    m1 m2))
 
-(defn ^:not-implemented contains-key
+(defn contains-key
   "Check wether the given map `m` contains given key `key`.
 
    You can use `contains` for that."
   [m key]
-  (…))
+  (m contains? key))
 
 (defn ^:not-implemented invert-map
   "given a map, invert the mapping, ie {:a :b, :c :d} should become {:d :c, :b :a}. if the value set contains duplicates, choose any mapping."
