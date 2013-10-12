e32fe11cc86a75c23561931ec6424987ca820f6a
Thomas Limp <thomas.limp@produktmacher.com>
2013-10-12 16:25:35 +0200

    state to recursion tasks

--- src/defworkshop/state_to_recursion.clj
+++ src/defworkshop/state_to_recursion.clj
@@ -3,51 +3,69 @@
 
 ;; More often than not, state can be avoided by using recursion. Let's see how it works
 
-(defn ^:not-implemented reverse-coll
+(defn reverse-coll
   "Reverse the collection `coll`.
 
    You can use `loop/recur` construct to loop over the sequence.
    `cons` prepends items to the list, try that out."
-  [coll]
-  (…))
+  ([coll]
+    (loop [c coll acc '()]
+      (if (empty? c)
+        acc
+        (recur (rest c) (cons (first c) acc))))))
 
-(defn ^:not-implemented recursive-sum
+(defn recursive-sum
   "We've already implemented sum using reduce, now let's move to implementing it via recursion!"
   [[head & tail]]
-  (…))
+  (if (empty? tail)
+    (or head 0)
+    (+ head (recursive-sum tail))))
 
-(defn ^:not-implemented recursive-sum-tc
+(defn recursive-sum-tc
   "with a tail-recursive version of sum, we can avoid stack overflows."
   ([coll]
      (recursive-sum-tc coll 0))
   ([[head & tail] acc]
-     (…)))
+      (if (nil? head)
+        acc
+        (recursive-sum-tc tail (+ acc head)))))
 
-(defn ^:not-implemented max-from-list
+(defn max-from-list
   "Get the maximum from list using recursion"
   [[head & tail]]
   (if (empty? tail)
     head
-    (…)))
+    (if (> head (first tail))
+      (max-from-list (cons head (rest tail)))
+      (max-from-list tail))))
 
 (defn ^:not-implemented my-reduce
   "generalizing the recursive sum example, write your own implementation of reduce! (for empty coll, just return nil.)"
   ([f [head & tail]]
      (my-reduce f head tail))
   ([f acc-init coll]
-     (…)))
+    (if (nil? acc-init)
+      nil
+      )))
 
-(defn ^:not-implemented max-from-list-tc
+(defn max-from-list-tc
   "Get the maximum from list using tail recursion (avoid stack overflow)"
   ([coll]
      (max-from-list-tc coll 0))
   ([[head & tail] max]
-     (…)))
+    (if (nil? head)
+      max
+      (if (> head max)
+        (max-from-list-tc tail head)
+        (max-from-list-tc tail max)))))
 
-(defn ^:not-implemented loop-recur-sum
+(defn loop-recur-sum
   "This implementation is somewhat easier to understand for people coming from imperative style."
   [numbers]
-  (…))
+  (loop [nums numbers acc 0]
+    (if (empty? nums)
+      acc
+      (recur (rest nums) (+ (first nums) acc)))))
 
 (defn ^:not-implemented map-from-keys-and-vals
   "Something that we've already implemented previously in terms of `zipmap`, but are going to implement again
